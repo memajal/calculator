674ecafc50f1cdb30d8f6d122aa9a9772878cb83
memajal <memajalketa92@gmail.com>
2024-06-05 18:39:47 +0200

    Create a function getFirstOperand() that for every number button clicked, will store the value to the firstOperand
    firstOperand is initialize as string and its final value after adding every button number value will be a string

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const numberButtons=document.querySelectorAll(".number");
 const operatorButtons=document.querySelectorAll(".operator");
 let display=document.querySelector("#display");
 
+let firstOperand="";
 
+function getFirstOperand() {
 numberButtons.forEach(button => {
     button.addEventListener('click' , () =>{
-
-        display.value=display.value+button.value;
+        firstOperand+=button.value;
+        display.value=firstOperand;
+        console.log(firstOperand);
     })
 });
+}
 
+getFirstOperand();
